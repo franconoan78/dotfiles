f470226943018a985d9b67fef5a5d40900ffd717
Charon Holueber <franconoan78@gmail.com>
2019-05-23 05:36:06 +0100

    modified:   install.sh 	modified:   vim/rc/init.vim 	modified:   vim/rc/options.vim 	bash/.bash_aliases 	bash/.bash_logout 	git/ 	profil 	vim/colorschems/ 	vim/rc/helpers.vim

--- install.sh
+++ install.sh
@@ -3,6 +3,13 @@
 GIT=${pwd}
 VIMSRC=${GIT}/vim
 VIMRC=${HOME}.vimrc
+BASH=~/.bashrc
+BASHALIAS=~/.bash_aliases
+BASHDIR=~/.bash
+
+export NVIM=/home/pi/.config/nvim
+
+
 echo $GIT2
 $!
 echo $1
@@ -11,22 +18,43 @@ echo `$GIT`
 pwd
 echo -e $GIT
 # check for old dotfiles and move them
-if [ -d .dotfiles ]; then
-        mv -fv ~/.dotfiles ~/.olddotfiles
-        mkdir ~/.dotfiles
-        mv -fv ~/.olddotfiles ~/.dotfiles
-    else
-        mkdir .dotfiles
-    fi
+function setup_vim(){
+if [ -d ~/.config/nvim  ]; then
+        rm -R ~/.config/nvim 
+fi
+
+mkdir ~/.config/nvim/plugged 
+mkdir ~/.config/nvim/vim_rc
+mkdir ~/.config/nvim/rc/plug_in_rc
+     ln  vim/init.vim   ~/.config/nvim/init.vim
+     ln  vim/rc/init.vim ~/.config/nvim/rc/init.vim 
+     ln  vim/rc/autocmd.vim   ~/.config/nvim/rc/autocmd.vim
+     ln  vim/rc/helpers.vim   ~/.config/nvim/rc/helpers.vim
+	 ln  vim/rc/options.vim   ~/.config/nvim/rc/options.vim
+	
+    # ln  ~/git/dotfiles/vim/rc/  ~/.config/nvim/rc/ 
+sudo cp vim/colorschems/*.vim /usr/share/nvim/runtime/colors/
+}
 
 
-if [ -f .vimrc ]; then
-        mv -fv ~/.vimrc ~/.dotfiles/
-        cp -fv ${GIT}vimrc ~/.vimrc
-    else
-        cp -fv ${GIT}vimrc ~/.vimrc
-    fi
+function setup_bash(){
+if [ -f ~/.bashrc ];then
+		echo "Remove Bashrc..."
+		rm ~/.bashrc
+		echo "Installing Bashrc..."
+		ln bash/bashrc ~/.bashrc
+fi
 
+if [  -d $BASHDIR ]; then
+		echo "Remove Bash..."
+		rm $BASHDIR
+		echo "Installing Bash..."
+		mkdir $BASHDIR
+		echo "Installing Aliases..."
+		ln bash/.bash_aliases $BASHDIR 
+		ln bash/.bash_logout ~/
 
+fi
+}
 
-# vim:tw=88:ts=4:ft=sh:norl:
+# vim:tw=88:ts=4:ft=sh:norl:foldenable
